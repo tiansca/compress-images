0d45c678ecd20f098503e0feabf5d441a74185d1
tianshicong <tianshicong@zbintel.net>
2025-04-08 13:57:47 +0800

    try agin

--- utils/saveFile.js
+++ utils/saveFile.js
@@ -40,92 +40,96 @@ const saveFile = (taskId, files, option) => {
   })
 }
 const compressImage = async (inputImagePath, targetImagePath, option, fileName, taskId, index, size) => {
-  const quality = Number(option.quality[0]) || 75
-  // 获取扩展名
+  return new Promise(async (resolve, reject) => {
+    const quality = Number(option.quality[0]) || 75
+    // 获取扩展名
 
-  let extendName = path.extname(targetImagePath).toLowerCase()
-  if (extendName) {
-    extendNameArr = extendName.split('.')
-    if (extendNameArr.length > 1) {
-      extendName = extendNameArr.at(-1)
-    }
-  }
-  let format = option.format[0] || extendName
-  let sharpObj
-  if (format === 'gif') {
-    sharpObj = sharp(inputImagePath, {animated: true})
-  } else {
-    sharpObj = sharp(inputImagePath)
-  }
-
-  // 旋转
-  if (Number(option.rotate[0])) {
-    sharpObj = await sharpObj.rotate(Number(option.rotate[0]))
-  }
-
-  // 如果是bmp或者svg，则转换为png
-  if (format === 'bmp' || format === 'svg') {
-    format = 'png'
-  }
-  if (format) {
-    switch (format) {
-      case 'png':
-        sharpObj = await sharpObj.png({quality: quality, palette: true, compressionLevel: 9})
-        break;
-      case 'webp':
-        sharpObj = await sharpObj.webp({quality: quality})
-        break;
-      case 'avif':
-        sharpObj = await sharpObj.avif({quality: quality})
-        break;
-      case 'jpg':
-      case 'jpeg':
-        sharpObj = await sharpObj.jpeg({quality: quality, progressive: true, mozjpeg: true})
-        break;
-      case 'tiff':
-        sharpObj = await sharpObj.tiff({quality: quality})
-        break;
-      case 'gif':
-        sharpObj = await sharpObj.gif({colors: Number(option.colors[0]), colours: Number(option.colours[1])})
-        break;
-    }
-  }
-  // 大小，如果有宽度并且没有高度
-  const width = option.width[0] ? Number(option.width[0]) : null
-  const height = option.height[0] ? Number(option.height[0]) : null
-  if (width && !height) {
-    console.log('宽度')
-    sharpObj = await sharpObj.resize({width})
-  } else if (height && !width) {
-    sharpObj = await sharpObj.resize({height})
-  } else if (width && height) {
-    sharpObj = await sharpObj.resize({width, height})
-  }
-  // 更改扩展名
-  targetImagePath = targetImagePath.replace(/\.[^.]+$/, `.${format}`)
-  sharpObj
-    .withMetadata(false)
-    .toFile(targetImagePath, async (err, info) => {
-      if (err) {
-        console.error('Error compressing image:', err);
-        sharpObj = null
-      } else {
-        console.log('Image compressed successfully:', info);
-        // 获取压缩后的文件大小
-        try {
-          const fileSize = await fs.promises.stat(targetImagePath)
-          JSONDB.setTaskStatus(taskId, {
-            name: fileName,
-            status: 'success',
-            size: size,
-            compressedSize: fileSize.size,
-            url: targetImagePath.split('public')[1] + '?' + Date.now() // 防止缓存
-          }, index)
-        } catch (e) {
-          console.error(e)
-        }
-        sharpObj = null
+    let extendName = path.extname(targetImagePath).toLowerCase()
+    if (extendName) {
+      extendNameArr = extendName.split('.')
+      if (extendNameArr.length > 1) {
+        extendName = extendNameArr.at(-1)
       }
-    });
+    }
+    let format = option.format[0] || extendName
+    let sharpObj
+    if (format === 'gif') {
+      sharpObj = sharp(inputImagePath, {animated: true})
+    } else {
+      sharpObj = sharp(inputImagePath)
+    }
+
+    // 旋转
+    if (Number(option.rotate[0])) {
+      sharpObj = await sharpObj.rotate(Number(option.rotate[0]))
+    }
+
+    // 如果是bmp或者svg，则转换为png
+    if (format === 'bmp' || format === 'svg') {
+      format = 'png'
+    }
+    if (format) {
+      switch (format) {
+        case 'png':
+          sharpObj = await sharpObj.png({quality: quality, palette: true, compressionLevel: 9})
+          break;
+        case 'webp':
+          sharpObj = await sharpObj.webp({quality: quality})
+          break;
+        case 'avif':
+          sharpObj = await sharpObj.avif({quality: quality})
+          break;
+        case 'jpg':
+        case 'jpeg':
+          sharpObj = await sharpObj.jpeg({quality: quality, progressive: true, mozjpeg: true})
+          break;
+        case 'tiff':
+          sharpObj = await sharpObj.tiff({quality: quality})
+          break;
+        case 'gif':
+          sharpObj = await sharpObj.gif({colors: Number(option.colors[0]), colours: Number(option.colours[1])})
+          break;
+      }
+    }
+    // 大小，如果有宽度并且没有高度
+    const width = option.width[0] ? Number(option.width[0]) : null
+    const height = option.height[0] ? Number(option.height[0]) : null
+    if (width && !height) {
+      console.log('宽度')
+      sharpObj = await sharpObj.resize({width})
+    } else if (height && !width) {
+      sharpObj = await sharpObj.resize({height})
+    } else if (width && height) {
+      sharpObj = await sharpObj.resize({width, height})
+    }
+    // 更改扩展名
+    targetImagePath = targetImagePath.replace(/\.[^.]+$/, `.${format}`)
+    sharpObj
+      .withMetadata(false)
+      .toFile(targetImagePath, async (err, info) => {
+        if (err) {
+          console.error('Error compressing image:', err);
+          sharpObj = null
+          resolve()
+        } else {
+          console.log('Image compressed successfully:', info);
+          // 获取压缩后的文件大小
+          try {
+            const fileSize = await fs.promises.stat(targetImagePath)
+            JSONDB.setTaskStatus(taskId, {
+              name: fileName,
+              status: 'success',
+              size: size,
+              compressedSize: fileSize.size,
+              url: targetImagePath.split('public')[1] + '?' + Date.now() // 防止缓存
+            }, index)
+          } catch (e) {
+            console.error(e)
+          }
+          sharpObj = null
+          resolve()
+        }
+      });
+  })
 }
 module.exports = saveFile
